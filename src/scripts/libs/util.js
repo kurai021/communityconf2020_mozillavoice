bd0d17cd515283e4cc0b53e811c91a2cddaa5b3f
Richard Armuelles <admin@kurai021.com>
2020-09-17 22:54:00 -0500

    first push

---
+++ src/scripts/libs/util.js
@@ -0,0 +1,71 @@
+export function changeBackground(){
+    const valuesSlide = document.querySelector(".values")
+    const imgBg = document.querySelector(".imgBg")
+    const existing = document.querySelector(".existing")
+
+    setInterval(function(){
+        if (valuesSlide.classList.contains("present") || imgBg.classList.contains("present") || existing.classList.contains("present")){
+            document.body.classList.add("alt-bg")
+        }
+        else {
+            document.body.classList.remove("alt-bg")
+        }
+    },10)
+}
+
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 500;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+ export function typing(){
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+ }
---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+import {initReveal} from "./libs/reveal.init.js"
+import {changeBackground,typing} from "./libs/util.js"
+
+window.onload = function(){
+    initReveal()
+    changeBackground()
+    typing()
+}
